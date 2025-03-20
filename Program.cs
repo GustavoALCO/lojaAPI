aadbc7b5606a9bb7914454c656073f0967dbaded
Gustavo Alves <119056348+GustavoALCO@users.noreply.github.com>
2025-03-20 10:51:45 -0300

    Começando Clean Architecture

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using FluentValidation;
-using loja_api.Context;
+﻿using loja_api.Context;
 using loja_api.EndpointsHandlers;
+using loja_api.ioc;
 using loja_api.Mapper.Cupom;
 using loja_api.Mapper.Emploree;
 using loja_api.Mapper.Storage;
@@ -10,85 +10,80 @@ using loja_api.Validators.Cupom;
 using loja_api.Validators.Employee;
 using loja_api.Validators.Storage;
 using loja_api.Validators.User;
-using MercadoPago.Config;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 
-MercadoPagoConfig.AccessToken = builder.Configuration["MercadoPago:AccessKey"];
-
-builder.Services.AddSwaggerGen(c =>
-{
-     
-
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Loja - API",
-        Version = "v1"
-    });
-
-    // 🔹 Configuração do JWT no Swagger
-    var securitySchema = new OpenApiSecurityScheme
-    {
-        Name = "Jwt Authentication",
-        Description = "Entre com o JWT Bearer",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
-
-    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securitySchema);
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {securitySchema, new string[] {} }
-    });
-});
+//MercadoPagoConfig.AccessToken = builder.Configuration["MercadoPago:AccessKey"];
+//
+//builder.Services.AddSwaggerGen(c =>
+//{
+//
+//
+//    c.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title = "Loja - API",
+//        Version = "v1"
+//    });
+//
+//    // 🔹 Configuração do JWT no Swagger
+//    var securitySchema = new OpenApiSecurityScheme
+//    {
+//        Name = "Jwt Authentication",
+//        Description = "Entre com o JWT Bearer",
+//        In = ParameterLocation.Header,
+//        Type = SecuritySchemeType.Http,
+//        Scheme = "bearer",
+//        BearerFormat = "JWT",
+//        Reference = new OpenApiReference
+//        {
+//            Id = JwtBearerDefaults.AuthenticationScheme,
+//            Type = ReferenceType.SecurityScheme
+//        }
+//    };
+//
+//    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securitySchema);
+//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+//    {
+//{securitySchema, new string[] {} }
+//    });
+//});
 
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        //para validar o Issuer
-        ValidateAudience = true,
-        //para valida a audience
-        ValidateLifetime = true,
-        //para verefica se esta no prazo de vida
-        ValidateIssuerSigningKey = true,
-        //informa que ira passar uma chave secreta
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-        ClockSkew = TimeSpan.Zero
-    };
-});
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//.AddJwtBearer(options =>
+//{
+//    options.TokenValidationParameters = new TokenValidationParameters()
+//    {
+//        ValidateIssuer = true,
+//        //para validar o Issuer
+//        ValidateAudience = true,
+//        //para valida a audience
+//        ValidateLifetime = true,
+//        //para verefica se esta no prazo de vida
+//        ValidateIssuerSigningKey = true,
+//        //informa que ira passar uma chave secreta
+//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//        ValidAudience = builder.Configuration["Jwt:Audience"],
+//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+//        ClockSkew = TimeSpan.Zero
+//    };
+//});
 
 //Conexão para o banco de dados 
-builder.Services.AddDbContext<ContextDB>(
-    o => o.UseSqlite(builder.Configuration.GetConnectionString("BdConnection"))
-);
+builder.Services.AddInfra(builder.Configuration);
 
 builder.Services.AddLogging();
 
@@ -96,22 +91,22 @@ builder.Services.AddLogging();
 builder.Services.AddScoped<GenerateToken>();
 
 //necessario para o AutoMapper Funcionar
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 //Declarando as Validações no código principal 
-builder.Services.AddScoped<IValidator<CupomCreateDTO>, CreateCupomValidation>();
-builder.Services.AddScoped<IValidator<CupomUpdateDTO>, UpdateCupomValidation>();
-builder.Services.AddScoped<IValidator<StorageCreateDTO>, CreateStorageValidation>();
-builder.Services.AddScoped<IValidator<StorageUpdateDTO>, UpdateStorageValition>();
-builder.Services.AddScoped<IValidator<CreateUserDTO>, CreateUserValidation>();
-builder.Services.AddScoped<IValidator<UserUpdateDTO>, UpdateUserValidation>();
-builder.Services.AddScoped<IValidator<EmployeeCreateDTO>, CreateEmployeeValidation>();
-builder.Services.AddScoped<IValidator<EmployeeUpdateDTO>, UpdateEmployeeValidation>();
+//builder.Services.AddScoped<IValidator<CupomCreateDTO>, CreateCupomValidation>();
+//builder.Services.AddScoped<IValidator<CupomUpdateDTO>, UpdateCupomValidation>();
+//builder.Services.AddScoped<IValidator<StorageCreateDTO>, CreateStorageValidation>();
+//builder.Services.AddScoped<IValidator<StorageUpdateDTO>, UpdateStorageValition>();
+//builder.Services.AddScoped<IValidator<CreateUserDTO>, CreateUserValidation>();
+//builder.Services.AddScoped<IValidator<UserUpdateDTO>, UpdateUserValidation>();
+//builder.Services.AddScoped<IValidator<EmployeeCreateDTO>, CreateEmployeeValidation>();
+//builder.Services.AddScoped<IValidator<EmployeeUpdateDTO>, UpdateEmployeeValidation>();
 
 builder.Services.AddScoped<CupomService>();
 builder.Services.AddScoped<HashService>();
-builder.Services.AddScoped<StorageServices>(); 
-builder.Services.AddScoped<UserServices>(); 
+builder.Services.AddScoped<StorageServices>();
+builder.Services.AddScoped<UserServices>();
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<MarketCartService>();
 builder.Services.AddScoped<MercadoPagoService>();
